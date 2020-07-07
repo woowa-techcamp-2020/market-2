2fee3160da26f523f3c13b956780d6a44a051d15
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-07 10:38:31 +0900

    add database configuration

--- config/vars.js
+++ config/vars.js
@@ -2,4 +2,5 @@ module.exports = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || "3000",
   logs: process.env.NODE_ENV === "production" ? "combined" : "dev",
+  migrate: false,
 };
