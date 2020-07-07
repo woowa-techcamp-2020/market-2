a881b63db17aff7ccde239d5be8308675ab87eab
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-07 09:39:54 +0900

    express 설정을 ./config/express.js 로 모듈화하였습니다.

--- server.js
+++ server.js
@@ -1,39 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const compression = require("compression");
-const helmet = require("helmet");
-const cors = require("cors");
-const path = require("path");
-const app = express();
+const app = require("./config/express");
+const logger = require("./config/logger");
+const { port, env } = require("./config/vars");
 
-app.set("port", process.env.PORT || 3000);
-
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-
-app.use(
-  bodyParser.json({
-    limit: "20mb",
-  })
-);
-app.use(
-  bodyParser.urlencoded({
-    limit: "20mb",
-    extended: true,
-  })
-);
-
-app.use(cors());
-app.use(helmet());
-app.use(compression());
-app.use(express.static("public"));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-app.use(require("./app/routes"));
-app.listen(app.get("port"), () =>
-  console.log("Listen on " + app.get("port") + " port")
+app.listen(port, () =>
+  logger.info("server started on port " + port + " (" + env + ")")
 );
 
 module.exports = app;
