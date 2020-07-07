4044213bf2a362b7c71a0ab3859a1a6f922f1c20
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-07 17:14:24 +0900

    remove read other files in route folder

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,32 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const routesPath = `${__dirname}/`;
-const { removeExtensionFromFile } = require("../middleware/utils");
-
-fs.readdirSync(routesPath).filter((file) => {
-  const routeFile = removeExtensionFromFile(file);
-  return routeFile !== "index" && routeFile !== "auth"
-    ? router.use(`/${routeFile}`, require(`./${routeFile}`))
-    : "";
-});
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
 router.get("/login", (req, res) => {
-  console.log("here render");
   res.render("html/page/login");
 });
 
 router.get("/register", (req, res) => {
-  console.log("here render");
   res.render("html/page/register");
 });
 
 router.get("/register_comp", (req, res) => {
-  console.log("here render");
   res.render("html/page/register_comp");
 });
 
