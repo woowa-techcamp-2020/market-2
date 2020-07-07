ee4c3cadffb5e90a40e754e62616e3cb7aeb31a6
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-07 10:39:27 +0900

    add db.service.start

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const app = require("./config/express");
+const dbService = require("./app/service/db.service");
 const logger = require("./config/logger");
-const { port, env } = require("./config/vars");
+const { port, env, migrate } = require("./config/vars");
 
-app.listen(port, () =>
-  logger.info("server started on port " + port + " (" + env + ")")
-);
+app.listen(port, () => {
+  logger.info("server started on port " + port + " (" + env + ")");
+  dbService(env, migrate).start();
+});
 
 module.exports = app;
