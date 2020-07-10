d4cd997c96e2315e9801b7eb3ae440077715c3ef
woowahan8 <woowahan8@MacBook-Pro.local>
2020-07-10 11:21:22 +0900

    union env for deploy heroku

---
+++ config/vars.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // env: process.env.NODE_ENV || "development",
+  // port: process.env.PORT || "3000",
+  // logs: process.env.NODE_ENV === "production" ? "combined" : "dev",
+  env: "development",
+  port: "3000",
+  logs: "dev",
+  migrate: false,
+  secret: "SeCrEtKeYfOrHaShInG",
+};
