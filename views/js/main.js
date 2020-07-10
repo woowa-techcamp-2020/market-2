f6164958f88ca3fb9b5648535b71f7040ef9825d
sss5793 <>
2020-07-10 14:02:29 +0900

    로그아웃 버튼 구현

--- views/js/main.js
+++ views/js/main.js
@@ -3,9 +3,17 @@ const goPageActions = (url) => {
   console.log("go page" + url);
 };
 
+const logout = () => {
+  localStorage.removeItem("fullname");
+  goPageActions("/");
+};
+
 const main = () => {
   const goLoginbtn = document.querySelector("#go_login");
   goLoginbtn.addEventListener("click", () => goPageActions("/login"));
+
+  const goLogoutbtn = document.querySelector("#go_logout");
+  goLogoutbtn.addEventListener("click", logout);
 
   const goRegisterbtn = document.querySelector("#go_register");
   goRegisterbtn.addEventListener("click", () => goPageActions("/register"));
@@ -14,14 +22,17 @@ const main = () => {
 
   const h2Text = document.querySelector("h2");
   const fullname = localStorage.getItem("fullname");
+
   if (fullname) {
     h2Text.innerText = `${fullname}님 환영합니다!`;
     goLoginbtn.style.display = "none";
+    goLogoutbtn.style.display = "block";
     rightElement.style.display = "none";
   } else {
     h2Text.innerText = "사장님, 로그인해주세요!";
     goLoginbtn.style.display = "inline-block";
     rightElement.style.display = "flex";
+    goLogoutbtn.style.display = "none";
   }
 };
 
